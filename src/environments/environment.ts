9f6df3596a06e01c431d03f5d356412fd63a0885
SuperJB <john.acb@gmail.com>
2019-09-17 20:24:13 -0700

    Adding missing files

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCvtsrpVnPHI_5ndKp2IkEiAr19XiBHxMI',
+    authDomain: 'tic-tac-toe-superjb.firebaseapp.com',
+    databaseURL: 'https://tic-tac-toe-superjb.firebaseio.com',
+    projectId: 'tic-tac-toe-superjb',
+    storageBucket: '',
+    messagingSenderId: '312366848402',
+    appId: '1:312366848402:web:5f86972fab74986c71894b'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
